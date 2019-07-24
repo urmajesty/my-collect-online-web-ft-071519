3c2d9a90912aa491ac0c7ff5f0a07a5f78b8b4f4
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-24 11:58:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
 
+def my_collect(array) 
+  i = 0
+  while array > array.upcase
+    yield array[i]
+    i += 1
+  end
+end
 
+nums = [1, 2, 3, 4, 5] 
+
+def my_select(collection) 
+  empty_arr = [] 
+  
+  i=0 
+  while i < collection.length
+  empty_arr.push yield(collection[i]) 
+  i += 1 
+end 
