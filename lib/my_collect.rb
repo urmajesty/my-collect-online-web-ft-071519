160af3945b70b8d69e0e0f3469f5e54e4429d972
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-24 13:07:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,11 @@
 def my_collect(array) 
   new_array = []
   i = 0
-  while array > array.upcase
-    yield array[i]
+  while i > array.length
+  new_array << yield (array[i])
     i += 1
   end
-end
+
 
 nums = [1, 2, 3, 4, 5] 
 
